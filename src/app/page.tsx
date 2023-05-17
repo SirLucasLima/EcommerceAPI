02a92508d82be20f58655d6aaa2b5f64f4d4d7f8
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-17 09:12:44 -0300

    background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+      </div>
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            You haven't recorded any memories yet, start{' '}
+            <a href="#" className="ubderline hover:text-gray-50">
+              {' '}
+              creating one now!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: 'var(--font-roboto)',
-        alt: 'var(--font-baiJamjure)',
+        alt: 'var(--font-bai-jamjuree)',
       },
 
       colors: {
@@ -49,6 +49,23 @@ module.exports = {
           800: '#027435',
           900: '#025929',
         },
+      },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      fontSize: {
+        '5xl': '2.5rem',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+
+      blur: {
+        full: '194px',
       },
     },
   },
