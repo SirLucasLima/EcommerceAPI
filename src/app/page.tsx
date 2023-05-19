6b4f44063144e913be75712f2526397582a32671
JoviTech <developerlucaslima@gmail.com>
2023-05-19 06:04:08 -0300

    class 3 done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+import { cookies } from 'next/headers'
+
 import { BlutStripes } from '@/components/BlurStripes'
 import { SignIn } from '@/components/SignIn'
 import { Hero } from '@/components/Hero'
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { Profile } from '@/components/Profile'
 
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <BlutStripes />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
