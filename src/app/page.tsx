ee8396fa6cbba1798997b99a4b04477ee3eb37e6
Developer Lucas Lima <developerlucaslima@gmail.com>
2023-05-21 11:25:09 -0300

    folder layoyt improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,5 @@
-import { cookies } from 'next/headers'
-
-import { BlutStripes } from '@/components/BlurStripes'
-import { SignIn } from '@/components/SignIn'
-import { Hero } from '@/components/Hero'
-import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
-import { Profile } from '@/components/Profile'
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        <BlutStripes />
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      {/* Right */}
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
